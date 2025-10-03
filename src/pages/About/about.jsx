f342d211ccd720288cd524d5084e9d28a1d7a290
menaka <shakya.menaka@gmail.com>
2025-10-03 15:43:18 +0200

    after correction collpasible

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,17 +1,53 @@
 import React from 'react'
 import { Banner } from '../../components/Banner/bannerAbout'
-//import bannerAboutImg from '../../assets/bannerAboutImg.png'
-//import styles from './about.module.css'
-//import React, { useState } from 'react'
+
 import { CCollapse } from '@coreui/react'
 import { FaChevronDown } from 'react-icons/fa'
 import Collapse from '../../components/Collapse/collapse'
 
 export default function About() {
+  const collapsibles = [
+    {
+      title: 'Fiabilité',
+      content:
+        'Les annonces postée sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes le informations sont régulièrement vérifiées par nos équipes.',
+    },
+    {
+      title: 'Respect',
+      content:
+        'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme.',
+    },
+    {
+      title: 'Service',
+      content:
+        'La qualité est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.',
+    },
+    {
+      title: 'Sécurité',
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux cirières de sécurité établis par nos services. En laissant une aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ]
   return (
     <main>
       <Banner />
-      <Collapse />
+
+      <Collapse
+        title={collapsibles[0].title}
+        content={collapsibles[0].content}
+      />
+      <Collapse
+        title={collapsibles[1].title}
+        content={collapsibles[1].content}
+      />
+      <Collapse
+        title={collapsibles[2].title}
+        content={collapsibles[2].content}
+      />
+      <Collapse
+        title={collapsibles[3].title}
+        content={collapsibles[3].content}
+      />
     </main>
   )
 }

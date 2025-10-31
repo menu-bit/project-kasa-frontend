14634c38ee79507d9ee91c85580c61d38ad6e877
Clément SABATTIÉ <clement.sabattie@gmail.com>
2025-10-31 20:25:52 +0100

    refactor Collapse component to use button for header and add accessibility attributes

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -1,26 +1,45 @@
-import { useState } from 'react'
+import { useState, useId } from 'react'
 import { FaChevronUp } from 'react-icons/fa'
+import PropTypes from 'prop-types'
 import styles from './collapse.module.css'
 
 export default function Collapse({ title, content, className = '' }) {
   const [open, setOpen] = useState(false)
   const classes = [styles.collapseContainer, className]
   const classNameCollapse = classes.join(' ')
+  const id = useId()
+  const contentId = `collapse-content-${id}`
+
+  const toggle = () => setOpen((prev) => !prev)
 
   return (
     <div className={classNameCollapse}>
-      <div className={styles.collapseHeader}>
+      <button
+        type="button"
+        className={styles.collapseHeader}
+        onClick={toggle}
+        aria-expanded={open}
+        aria-controls={contentId}
+      >
         <span>{title}</span>
         <FaChevronUp
           className={`${styles.icon} ${open ? styles.iconOpen : ''}`}
-          onClick={() => setOpen((prev) => !prev)}
         />
-      </div>
+      </button>
 
-      <div className={`${styles.collapseContent} ${open ? styles.open : ''}`}>
+      <div
+        id={contentId}
+        className={`${styles.collapseContent} ${open ? styles.open : ''}`}
+      >
         {/** boucle for sur content https://fr.react.dev/learn#rendering-lists */}
         <div className={styles.innerContent}>{content}</div>
       </div>
     </div>
   )
+}
+
+Collapse.propTypes = {
+  title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  content: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  className: PropTypes.string,
 }
